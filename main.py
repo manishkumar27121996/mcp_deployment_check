4216cc06b5eaa4182bf1b5ef1966a579f4e7c5ae
Manish Kumar <82048889+manishkumar27121996@users.noreply.github.com>
2025-06-16 14:19:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ from typing import Any
 import httpx
 from fastmcp import FastMCP
 import os 
-from fastapi import FastAPI
+#from fastapi import FastAPI
 # Initialize FastMCP server
 mcp = FastMCP("weather")
 
@@ -116,18 +116,18 @@ Forecast: {period['detailedForecast']}
         forecasts.append(forecast)
 
     return "\n---\n".join(forecasts)
-#app = mcp.streamable_http_app()
-http_app = mcp.http_app()                         # Streamable HTTP (/mcp)
-sse_app = mcp.http_app(transport="sse")
+app = mcp.streamable_http_app()
+# http_app = mcp.http_app()                         # Streamable HTTP (/mcp)
+# sse_app = mcp.http_app(transport="sse")
 
-app = FastAPI(lifespan=http_app.lifespan)
-app.mount("/mcp", http_app)
-app.mount("/sse", sse_app)
+# app = FastAPI(lifespan=http_app.lifespan)
+# app.mount("/mcp", http_app)
+# app.mount("/sse", sse_app)
 
 
 if __name__ == "__main__":
     # Initialize and run the server
     import uvicorn
-    #mcp.run(transport="streamable-http", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT",8000)))
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    #uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT",8000)))
 
